dbb5f4a04f0fdad78214154bc8f064d933e1bff0
DevTester <jgh0721@gmail.com>
2013-07-28 20:50:49 +0900

    Initial create localization project( Radialix 3 )

---
+++ Tools/build.bat
@@ -0,0 +1,7 @@
+halibut.exe config.but intro_K.but tutorial_K.but usage_K.but script_K.but var_K.but labels_k.but jumps_K.but pages_K.but sections_K.but usection_K.but functions_K.but callback_K.but attributes_K.but compilerflags_K.but basic_K.but registry_K.but generalpurpose_K.but flowcontrol_K.but file_K.but uninstall_K.but misc_K.but string_K.but stack_K.but int_k.but reboot_K.but log_K.but sec_K.but ui_K.but langs_K.but plugin_K.but silent_K.but compiler_K.but defines_K.but modernui_K.but library_K.but usefulfunc_K.but usefulinfos_K.but headers.but history_K.but build.but credits.but license.but | more
+
+hhc nsis.hhp
+del *.html
+del ind.hhk toc.hhc
+
+pause
